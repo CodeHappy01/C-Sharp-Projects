7daf59540e360d409532e77010541c19f8b972e0
Bradley Robles <bradleyrobles88@gmail.com>
2022-11-19 19:18:03 -0600

    Add project files.

---
+++ WelcomePageAccountingSystems/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme accounting Systems\nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
