155e58e6c11dadee29b45fc0400a5f98ee5c8fd4
Bradley Robles <bradleyrobles88@gmail.com>
2022-11-29 00:01:52 -0600

    ...

---
+++ Assignments-and-Projects/MethodSubmissionAssignment2/MethodSubmissionAssignment2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment2
+{
+    class Class1
+    {
+        //Method (take one parameter)
+        public static string MyMethod(string x)
+        {
+            int result = Int32.Parse(x) + Int32.Parse("123");
+            return result.ToString();
+        }
+
+        //Method (take two parameters)
+        public static string MyMethod(string x, string y = "0")
+        {
+            int result = Int32.Parse(x) + Int32.Parse("123") + Int32.Parse(y);
+            return result.ToString();
+        }
+    }
+}
---
+++ Assignments-and-Projects/MethodSubmissionAssignment2/MethodSubmissionAssignment2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine("Type a whole number:");
+            string userInput1 = Console.ReadLine();
+            //Asks user whether they want to add second number
+            Console.WriteLine("Would you like to add another number? (yes/no)");
+            string userAnswer = Console.ReadLine();
+            int otherNumber = 123;
+
+            if (userAnswer == "yes")
+            {
+                ////Sends two arguments to MyMethod
+                Console.WriteLine("Type another whole number:");
+                string userInput2 = Console.ReadLine();
+                string userOutput = Class1.MyMethod(userInput1, userInput2);
+                Console.WriteLine(userInput1 + " + " + otherNumber + " + " + userInput2 + " = " + userOutput);
+            }
+
+            else if (userAnswer == "no")
+            {
+                //Only sends one argument to MyMethod
+                string userOutput = Class1.MyMethod(userInput1);
+                Console.WriteLine(userInput1 + " + " + otherNumber + " = " + userOutput);
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry, wrong input");
+            }
+            Console.ReadLine();
+        }
+    }
+}
