38bcc495f92e09305f807af9697731bae9e5cb04
Bradley Robles <bradleyrobles88@gmail.com>
2022-11-28 21:43:42 -0600

    Finished Requested Actions

---
+++ Assignments-and-Projects/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Class1
+    {
+        //First Method 
+        public static int MyMethod(int x)
+        {
+            return x * 321;
+        }
+        //Second Method
+        public double MyMethod(double y)
+        {
+            return y + 5.5;
+        }
+        //Third Method
+        public int MyMethod(string z)
+        {
+            return Int32.Parse(z) + 10;
+            
+        }
+    }
+    }
---
+++ Assignments-and-Projects/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiated class
+            Class1 Class1 = new Class1();
+            //First Method Variable
+            int multMethod = Class1.MyMethod(123);
+            //Second Method Variable
+            double subMethod = Class1.MyMethod(y: 55);
+            //Third Method Varible
+            int addMethod = Class1.MyMethod("321");
+
+            Console.WriteLine(multMethod);
+            Console.WriteLine(subMethod);
+            Console.WriteLine(addMethod);
+            Console.ReadLine();
+
+
+
+
+
+            //{
+            //    static void Main(string[] args)
+            //    {
+            //        Deck deck = new Deck();
+            //        int timesShuffled = 0;
+            //        deck = Shuffle(deck: deck, out timesShuffled, times: 3);
+
+            //        foreach (Card card in deck.Cards)
+            //        {
+            //            Console.WriteLine(card.Face + " of " + card.Suit);
+            //        }
+            //        Console.WriteLine(deck.Cards.Count);
+            //        Console.WriteLine("Times shuffled: {0}", timesShuffled);
+            //        Console.ReadLine();
+            //    }
+
+            //    public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
+            //    {
+            //        timesShuffled = 0;
+            //        for (int i = 0; i < times; i++)
+            //        {
+            //            timesShuffled++;
+            //            List<Card> TempList = new List<Card>();
+            //            Random random = new Random();
+
+            //            while (deck.Cards.Count > 0)
+            //            {
+            //                int randomIndex = random.Next(0, deck.Cards.Count);
+            //                TempList.Add(deck.Cards[randomIndex]);
+            //                deck.Cards.RemoveAt(randomIndex);
+            //            }
+            //            deck.Cards = TempList;
+            //        }
+
+            //        return deck;
+
+            //    }
+            //public static Deck Shuffle(Deck deck, int times) 
+            //{
+            //    for (int i = 0; i < times; i++)
+            //    {
+            //        deck = Shuffle(deck);   
+            //    }
+            //    return deck;
+
+            //}
+
+
+        } 
+    }
+}
