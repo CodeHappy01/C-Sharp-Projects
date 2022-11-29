463d5ad86c397146aa691df635fc777435092743
Bradley Robles <bradleyrobles88@gmail.com>
2022-11-29 01:46:23 -0600

    .

---
+++ Assignments-and-Projects/MethodClassSubmissionAssignment3/MethodClassSubmissionAssignment3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment3
+{
+    class Class1
+    {
+        //Method (takes two parameters)
+        public void MyMethod(int x, int y = 0)
+        {
+            var result1 = x + 321;
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ Assignments-and-Projects/MethodClassSubmissionAssignment3/MethodClassSubmissionAssignment3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.MyMethod(1, 2);
+            Console.ReadLine();
+        }
+    }
+}
